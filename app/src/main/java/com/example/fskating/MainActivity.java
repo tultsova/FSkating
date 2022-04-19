b72a9d4fd8a90eb57145bdfdcc2cba3c065b68eb
tultsova <nastyatyltsova@gmail.com>
2022-04-19 15:45:23 +0300

    update all xml

--- app/src/main/java/com/example/fskating/MainActivity.java
+++ app/src/main/java/com/example/fskating/MainActivity.java
@@ -1,20 +1,32 @@
 package com.example.fskating;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private TextView mSelectText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
 
-      /*  VideoView videoRt = findViewById(R.id.video_rt);
+        /*GridView gridview = (GridView) findViewById(R.id.grid);
+        gridview.setAdapter(new ImageAdapter(this));*/
+
+    }
+
+        /*VideoView videoRt = findViewById(R.id.video_rt);
         String videoPathRt = "android.resource://" + getPackageName() + "/" + R.raw.video_rt;
         Uri videoRtUri = Uri.parse(videoPathRt);
         videoRt.setVideoURI(videoRtUri);
@@ -31,5 +43,5 @@ public class MainActivity extends AppCompatActivity {
         MediaController mediaController2 = new MediaController(this);
         videoPt.setMediaController(mediaController2);
         mediaController2.setAnchorView(videoPt);*/
-    }
+
 }
